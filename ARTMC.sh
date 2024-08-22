a816b05173fddd76ecf1d2f344a8eeaf6805f819
Sunil <skswain172@gmail.com>
2024-08-22 10:22:02 +0000

    d1

---
+++ ARTMC.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Arithmatic Operations
+#Author-Sunil
+#date-21.08.2024
+echo $0
+echo $1
+echo $2
+echo $3
+echo $4
+echo $5
+
+let sum=$1+$2
+let sub=$1-$2
+let mult=$1*$2
+let div=$1/$2
+
+echo "The sum is $sum"
+echo "The substraction is $sub"
+echo "The multiplication is $mult"
+echo "The division is $div"
